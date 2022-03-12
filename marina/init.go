9da15cb2d72f83789e61dd9c7939011a9a85a01a
Rajesh Battala <rajesh.battala@nutanix.com>
2022-03-12 09:17:58 +0530

    Added gflags to support port configurations, added init methods

---
+++ marina/init.go
@@ -0,0 +1,47 @@
+/*
+Copyright (c) 2022 Nutanix Inc. All rights reserved.
+
+Author: rajesh.battala@nutanix.com
+
+Initialize Marian, including:
+ - gflags
+ - OpenTelemetry Tracing.
+ - HostAddress - Resolving PCIP.
+
+*/
+
+package main
+
+import (
+	"github.com/nutanix-core/content-management-marina/common"
+	utils "github.com/nutanix-core/content-management-marina/util"
+	"github.com/nutanix-core/ntnx-api-utils-go/tracer"
+	otelSdkTrace "go.opentelemetry.io/otel/sdk/trace"
+	log "k8s.io/klog/v2"
+	"net"
+)
+
+var traceProvider *otelSdkTrace.TracerProvider
+
+// initGflags initialize gflags for Marina.
+func initFlags() {
+	common.FlagsInit()
+}
+
+func initHostIP() {
+	pcIP, err := net.LookupIP(utils.PcFQDN)
+	if err != nil || len(pcIP) == 0 {
+		log.Fatal("Could not fetch host IP to initialise service")
+	}
+
+	utils.HostAddr = pcIP[0].String()
+	log.Info("Setting HostAddr to : ", utils.HostAddr)
+}
+func initOpenTelemetryTracing() {
+	traceProvider, err := tracer.InitTracer(utils.ServiceName)
+	if traceProvider != nil {
+		log.Errorf("Error while initializing tracer: %v ", err.Error())
+	} else {
+		log.Info("OpenTelemetry Tracer got initialized %v", traceProvider)
+	}
+}
